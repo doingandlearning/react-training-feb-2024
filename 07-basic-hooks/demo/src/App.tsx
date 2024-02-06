0787a98e77bf57ae7ea84a37270944fe9d2a9f82
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-02-06 10:26:22 +0000

    adds implemetation

--- 07-basic-hooks/demo/src/App.tsx
+++ 07-basic-hooks/demo/src/App.tsx
@@ -1,42 +1,51 @@
 import React from "react"
 import './App.css'
 import Temp from "./component/Temp";
+import Gallery from "./component/Gallery";
 
 function App() {
-  const [counter, setCounter] = React.useState(0);
-  const [counter2, setCounter2] = React.useState(0);
+  // const [counter, setCounter] = React.useState(0);
+  // const [counter2, setCounter2] = React.useState(0);
 
-  // componentDidMount + componentDidUpdate / componentDidRerender
-  React.useEffect(() => {
-    console.log("On every render: Use effect is called.")
-    return () => {
-      console.log("Every render  cleanup")
-    }
-  })
+  // // componentDidMount + componentDidUpdate / componentDidRerender
+  // React.useEffect(() => {
+  //   console.log("On every render: Use effect is called.")
+  //   return () => {
+  //     console.log("Every render  cleanup")
+  //   }
+  // })
 
-  // componentDidMount ./
-  React.useEffect(() => {
-    // Initial data fetch!
-    console.log("On initial render: Use effect is called.")
-    return () => {
-      console.log("Initial only cleanup")
-    }
-  }, []) // Empty Dependency array - means initial render only!
+  // // componentDidMount ./
+  // React.useEffect(() => {
+  //   // Initial data fetch!
+  //   console.log("On initial render: Use effect is called.")
+  //   return () => {
+  //     console.log("Initial only cleanup")
+  //   }
+  // }, []) // Empty Dependency array - means initial render only!
 
-  // componentDidMount + dependencyDidChange
-  React.useEffect(() => {
-    // Updated data ... 
-    console.log("On counter 1 only: Use effect is called.")
-    return () => {
-      console.log("Counter 1 only cleanup")
-    }
-  }, [counter])
+  // // componentDidMount + dependencyDidChange
+  // React.useEffect(() => {
+  //   // Updated data ... 
+  //   console.log("On counter 1 only: Use effect is called.")
+  //   return () => {
+  //     console.log("Counter 1 only cleanup")
+  //   }
+  // }, [counter])
 
 
 
   return (
     <>
-      <p>
+      <Gallery baseAddress="http://olsensoft.com/thumbnails" urls={["alesund.jpg",
+        "bronnoysund.jpg",
+        "budapest.jpg",
+        "capetown.jpg",
+        "cork.jpg",
+        "joburghadeda.jpg",
+        "oslo.jpg",
+        "singapore.jpg",]} />
+      {/* <p>
         Counter 1: {counter}
       </p>
       <button onClick={() => setCounter(counter + 1)}>Click</button>
@@ -46,7 +55,7 @@ function App() {
       <button onClick={() => setCounter2(counter2 + 1)}>Click</button>
       <div v-if="counter === 1">
         {counter === 1 && <Temp />}
-      </div>
+      </div> */}
     </>
   )
 }
